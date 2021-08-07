182254f1e55f8db8678cfb12ab540f81c710f3c1
timothysepulvado <timothysepulvado@yahoo.com>
2021-08-07 15:04:08 -0700

    working on things

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,13 @@
 
 
 var Cryptourl = "https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD"
+
+async function displayFiveDayForecast() {
+
+    var queryURL = "https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD"
+
+   
+
+fetch('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD')
+  .then(response => response.json())
+  .then(data => console.log(data));}
