0d6d02c1ddaf114346aa03790e159653086e3d53
timothysepulvado <timothysepulvado@yahoo.com>
2021-08-08 22:34:19 -0700

    api

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -11,3 +11,19 @@ async function displayFiveDayForecast() {
 fetch('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD')
   .then(response => response.json())
   .then(data => console.log(data));}
+
+  fetch("https://coinranking1.p.rapidapi.com/exchanges", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "coinranking1.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+  
+  
