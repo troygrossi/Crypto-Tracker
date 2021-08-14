5360fa0929cf3c96d08a2c3e3cf8cde16d29325b
Sam Morgen <samorgen14@gmail.com>
2021-08-13 19:35:32 -0700

    check if logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,4 @@
 const router = require("express").Router();
-// const data = require("../public/javascript/crypto.js");
-// console.log(data);
 const test = { test: "test" };
 router.get("/", (req, res) => {
   res.render("home", { test });
